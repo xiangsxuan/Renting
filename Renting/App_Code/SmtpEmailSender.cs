ef568192ce296017e6e2fd5be4d5040d4fa79683
xiangsxuan <598582847@qq.com>
2019-05-01 17:08:58 +0800

    实现邮箱发送类

---
+++ Renting/App_Code/SmtpEmailSender.cs
@@ -0,0 +1,32 @@
+﻿using Renting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Web;
+
+/// <summary>
+/// sendMail 的摘要说明
+/// </summary>
+public class sendMail
+{
+    public  bool SendEmailAsync(string receiveEmail, string subject, string message)
+    {
+        using (SmtpClient client = new SmtpClient(MailConfiguration.Host, MailConfiguration.Port))
+        {
+            client.Credentials = new NetworkCredential(MailConfiguration.Sender, MailConfiguration.Password);
+            client.EnableSsl = true;
+
+            MailMessage mail = new MailMessage(MailConfiguration.Sender, receiveEmail, subject, message);
+            mail.SubjectEncoding = Encoding.UTF8;
+            mail.BodyEncoding = Encoding.UTF8;
+            mail.IsBodyHtml = true;
+
+            client.SendMailAsync(mail);
+            return true;
+        }
+    }
+}
+
