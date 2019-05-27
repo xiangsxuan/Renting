d37db05fafed6d730bc1fd07e99eaea49bd4de08
xiangsxuan <598582847@qq.com>
2019-05-27 14:08:34 +0800

    添加邮箱配置类

---
+++ Renting/App_Code/MailConfiguration.cs
@@ -0,0 +1,11 @@
+namespace Renting
+{
+    public static class MailConfiguration
+    {
+        public const int Port = 587;
+        public const bool EnableSsl = true;
+        public const string Host = "smtp.qq.com";
+        public const string Password = "qqÊÚÈ¨Âë";
+        public const string Sender = "qqÓÊÏä";
+    }
+}
